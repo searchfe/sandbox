5c41077d53b267fbeb7f88574758e4bf342c7eeb
harttle <yangjvn@126.com>
2018-04-17 20:49:37 +0800

    coverall

--- src/apis/fetch.js
+++ src/apis/fetch.js
@@ -1,14 +1,8 @@
 define(function (require) {
     return function (sandbox) {
-        if (!window.fetch) {
-            var msg = 'Fetch API not implemented by your browser, ' +
-                'consider a polyfill: https://www.npmjs.com/package/whatwg-fetch';
-            console.log(msg);
-            return;
-        }
-        return function fetch (input, init) {
+        return function (input, init) {
             return new Promise(function (resolve, reject) {
-                window.fetch(input, init)
+                fetch(input, init)
                 .then(sandbox.delegate.untilRunning(function (result) {
                     resolve(result);
                 }))
