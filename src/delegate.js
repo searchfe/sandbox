a7d5cb54700f14e69201612330f457605b929ad5
zhangzijie <zhangzijie@baidu.com>
2018-11-14 17:04:50 +0800

    fix: fix wrong arguments use in delegate.js

--- src/delegate.js
+++ src/delegate.js
@@ -5,18 +5,20 @@ define(function (require) {
         return {
             untilRunning: function (cb, thisArg) {
                 return function () {
+                    var args = arguments;
                     if (sandbox.state === states.RUNNING) {
-                        return cb.apply(thisArg, arguments);
+                        return cb.apply(thisArg, args);
                     }
                     sandbox.on('run', function () {
-                        return cb.apply(thisArg, arguments);
+                        return cb.apply(thisArg, args);
                     });
                 };
             },
             onlyRunning: function (cb, thisArg) {
                 return function () {
+                    var args = arguments;
                     if (sandbox.state === states.RUNNING) {
-                        return cb.apply(thisArg, arguments);
+                        return cb.apply(thisArg, args);
                     };
                 };
             }
